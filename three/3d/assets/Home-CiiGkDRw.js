7dd372361651aa3341d35ba0f47aded2d19cad33
hao <949145820@qq.com>
2024-03-07 17:56:22 +0800

    update

---
+++ three/3d/assets/Home-CiiGkDRw.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Dcb1ZePP.js";const r=()=>e.jsxs("div",{children:[e.jsx("h2",{children:"Home"}),e.jsx("p",{children:"Welcome to the Home page"})]});export{r as default};
---
+++ three/3d/assets/Login-CJjxZjcM.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Dcb1ZePP.js";const n=()=>s.jsxs("div",{children:[s.jsx("h2",{children:"Login"}),s.jsx("p",{children:"Login us page"})]});export{n as default};
