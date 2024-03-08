7941423d70c000d7224a042965471b11c9867fe4
hao <949145820@qq.com>
2024-03-08 09:32:52 +0800

    1

---
+++ three/3d/assets/Home-B3xadRvC.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-C1fLXkXM.js";const r=()=>e.jsxs("div",{children:[e.jsx("h2",{children:"Home"}),e.jsx("p",{children:"Welcome to the Home page"})]});export{r as default};
---
+++ three/3d/assets/Login-CATjmNg5.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-C1fLXkXM.js";const n=()=>s.jsxs("div",{children:[s.jsx("h2",{children:"Login"}),s.jsx("p",{children:"Login us page"})]});export{n as default};
