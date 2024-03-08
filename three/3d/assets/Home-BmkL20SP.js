ca160254ef8a4adfe3244edb7328c920ec252950
hao <949145820@qq.com>
2024-03-08 09:17:37 +0800

    1

---
+++ three/3d/assets/Home-BmkL20SP.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Cm-K6Kes.js";const r=()=>e.jsxs("div",{children:[e.jsx("h2",{children:"Home"}),e.jsx("p",{children:"Welcome to the Home page"})]});export{r as default};
---
+++ three/3d/assets/Login-CFogZm-d.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Cm-K6Kes.js";const n=()=>s.jsxs("div",{children:[s.jsx("h2",{children:"Login"}),s.jsx("p",{children:"Login us page"})]});export{n as default};
